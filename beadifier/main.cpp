e900da9dc241a89a9a46b29851b736919ed0bba4
Lotes <markhoernchen@gmx.de>
2015-12-29 13:53:56 +0100

    add csgjs.cpp

--- beadifier/main.cpp
+++ beadifier/main.cpp
@@ -4,35 +4,10 @@
 #include <fstream>
 #include <list>
 #include <sstream>
+#include <math.h>
+#include "csgjs.h"
  
 using namespace std;
-
-double toDouble(const string& s) {
-	istringstream i(s);
-	double x;
-	if (!(i >> x))
-		return 0;
-	return x;
-}
-
-int toInt(const string& s) {
-	istringstream i(s);
-	int x;
-	if (!(i >> x))
-		return 0;
-	return x;
-}
-
-template<class T>
-struct Vector2 {
-public:
-	T x;
-	T y;
-	Vector2(T x, T y) {
-		this->x = x;
-		this->y = y;
-	}
-};
 
 template<class T>
 struct Vector3 {
@@ -40,10 +15,50 @@ public:
 	T x;
 	T y;
 	T z;
+	Vector3(): x(0), y(0), z(0) {}
 	Vector3(T x, T y, T z) {
 		this->x = x;
 		this->y = y;
 		this->z = z;
+	}
+	Vector3<T> multiplyScalar(const T scalar) const {
+		Vector3<T> result(x*scalar, y*scalar, z*scalar);
+		return result;
+	}
+	Vector3<T> add(const Vector3<T>& other) const {
+		Vector3<T> result(x+other.x, y+other.y, z+other.z);
+		return result;
+	}
+	Vector3<T> subtract(const Vector3<T>& other) const {
+		Vector3<T> result(x-other.x, y-other.y, z-other.z);
+		return result;
+	}
+	double length() const {
+		return sqrt(x*x + y*y + z*z);
+	}
+	Vector3<double> normalize() const {
+		double len = length();
+		if(len == 0) {
+			Vector3<double> result(0, 0, 0);
+			return result;
+		} else {
+			Vector3<double> result(x/len, y/len, z/len);
+			return result;
+		}
+	}
+	Vector3<T> cross(const Vector3<T>& vertex) const {
+		Vector3<T> result(
+			y * vertex.z - z * vertex.y,
+			z * vertex.x - x * vertex.z,
+			x * vertex.y - y * vertex.x
+		);
+		return result;
+	}
+	T dot(const Vector3<T>& other) const {
+		return x*other.x + y*other.y + z*other.z;
+	}
+	Vector3<T> lerp(const Vector3<T> a, const T scalar) const {
+		return add(a.subtract(*this).multiplyScalar(scalar));
 	}
 };
 
@@ -63,24 +78,49 @@ public:
 				addVertex(match[1].str(), match[2].str(), match[3].str());
 			} 
 			else if(boost::regex_match(line, match, facePattern1)) {
-				cout << "FACE1: " << line << endl;
 				addFace(match[1].str(), match[2].str(), match[3].str());
 			}
 			else if(boost::regex_match(line, match, facePattern2)) {
-				cout << "FACE2: " << line << endl;
 				addFace(match[1].str(), match[2].str(), match[3].str());
 			}
 			else if(boost::regex_match(line, match, facePattern3)) {
-				cout << "FACE3: " << line << endl;
 				addFace(match[1].str(), match[2].str(), match[3].str());
 			}
 			else if(boost::regex_match(line, match, facePattern4)) {
-				cout << "FACE4: " << line << endl;
 				addFace(match[1].str(), match[2].str(), match[3].str());
 			}
 		}
 	}
+	csgjs_model toModel() {
+		csgjs_model result;
+		for(list<Vector3<double>>::iterator it=vertices.begin(); it!=vertices.end(); it++) {
+			csgjs_vertex vertex;
+			csgjs_vector position(it->x, it->y, it->z);
+			vertex.pos = position;
+			result.vertices.push_back(vertex);
+		}
+		for(list<Vector3<int>>::iterator it=faces.begin(); it!=faces.end(); it++) {
+			result.indices.push_back(it->x);
+			result.indices.push_back(it->y);
+			result.indices.push_back(it->z);
+		}
+		return result;
+	}
 private:
+	double toDouble(const string& s) {
+		istringstream i(s);
+		double x;
+		if (!(i >> x))
+			return 0;
+		return x;
+	}
+	int toInt(const string& s) {
+		istringstream i(s);
+		int x;
+		if (!(i >> x))
+			return 0;
+		return x;
+	}
 	void addVertex(const string& xs, const string& ys, const string& zs) {
 		double x = toDouble(xs);
 		double y = toDouble(ys);
@@ -89,9 +129,9 @@ private:
 		vertices.push_back(v);
 	}
 	void addFace(const string& as, const string& bs, const string& cs) {
-		int a = toInt(as);
-		int b = toInt(bs);
-		int c = toInt(cs);
+		int a = toInt(as)-1;
+		int b = toInt(bs)-1;
+		int c = toInt(cs)-1;
 		Vector3<int> f(a, b, c);
 		faces.push_back(f);
 	}
@@ -101,5 +141,6 @@ private:
 
 int main() {
 	Object obj("../models/pikachu.obj");
+	obj.toModel();
 	return 0;
 }
